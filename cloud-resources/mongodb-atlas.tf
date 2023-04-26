8d6358aae7c28ae5c200760590e46e1ba22f5abe
Julius Tens <mail@juliustens.eu>
2023-04-27 01:33:27 +0200

    add serverless mongodb instance

--- cloud-resources/mongodb-atlas.tf
+++ cloud-resources/mongodb-atlas.tf
@@ -9,12 +9,19 @@ provider "mongodbatlas" {
 }
 
 locals {
-  mongodbatlas_cluster_name = "crimson-cluster"
+  mongodbatlas_cluster_name    = "crimson-cluster"
+  mongodbatlas_serverless_name = "crimson-serverless"
 }
 
-# you can retrieve the base database uri by running `terraform output --raw crimson_uri`
-output "crimson_uri" {
+# you can retrieve the base database uri by running `terraform output --raw crimson_cluster_uri`
+output "crimson_cluster_uri" {
   value     = mongodbatlas_cluster.crimson_cluster.mongo_uri_with_options
+  sensitive = true
+}
+
+# you can retrieve the base database uri by running `terraform output --raw crimson_serverless_uri`
+output "crimson_serverless_uri" {
+  value     = mongodbatlas_serverless_instance.crimson_serverless.connection_strings_standard_srv
   sensitive = true
 }
 
@@ -53,6 +60,21 @@ resource "mongodbatlas_cluster" "crimson_cluster" {
   }
 }
 
+resource "mongodbatlas_serverless_instance" "crimson_serverless" {
+  project_id = mongodbatlas_project.crimson.id
+  name       = local.mongodbatlas_serverless_name
+
+  provider_settings_backing_provider_name = "AZURE"
+  provider_settings_region_name           = "WESTEUROPE"
+  provider_settings_provider_name         = "SERVERLESS"
+  continuous_backup_enabled               = true
+  termination_protection_enabled          = true
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
 resource "mongodbatlas_database_user" "chore_score_bot_user" {
   username           = "chore-score-bot-user"
   password           = var.mongodbatlas_chore_score_bot_user_password
@@ -71,6 +93,11 @@ resource "mongodbatlas_database_user" "chore_score_bot_user" {
 
   scopes {
     name = local.mongodbatlas_cluster_name
+    type = "CLUSTER"
+  }
+
+  scopes {
+    name = local.mongodbatlas_serverless_name
     type = "CLUSTER"
   }
 }
