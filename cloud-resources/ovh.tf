0da89c1156059376f55410d3875fa70beb52f8ec
Julius Tens <mail@juliustens.eu>
2023-07-09 01:11:45 +0200

    fix: update k8s version

--- cloud-resources/ovh.tf
+++ cloud-resources/ovh.tf
@@ -20,7 +20,7 @@ resource "ovh_cloud_project_kube" "orchid_cluster" {
   service_name = var.ovh_project_id
   name         = "orchid"
   region       = "SBG5"
-  version      = "1.25"
+  version      = "1.26"
 
   lifecycle {
     prevent_destroy = true
