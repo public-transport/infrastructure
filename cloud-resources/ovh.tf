c6989108c2cacc2391a3054fd1c1609ece863c78
Julius Tens <mail@juliustens.eu>
2023-09-18 22:21:56 +0200

    chore: delete ovh cluster 😮

--- cloud-resources/ovh.tf
+++ cloud-resources/ovh.tf
@@ -3,43 +3,9 @@ variable "ovh_application_secret" {}
 variable "ovh_consumer_key" {}
 variable "ovh_project_id" {}
 
-# you can retrieve the kubeconfig file by running `terraform output --raw orchid_kubeconfig`
-output "orchid_kubeconfig" {
-  value     = ovh_cloud_project_kube.orchid_cluster.kubeconfig
-  sensitive = true
-}
-
 provider "ovh" {
   endpoint           = "ovh-eu"
   application_key    = var.ovh_application_key
   application_secret = var.ovh_application_secret
   consumer_key       = var.ovh_consumer_key
-}
-
-resource "ovh_cloud_project_kube" "orchid_cluster" {
-  service_name = var.ovh_project_id
-  name         = "orchid"
-  region       = "SBG5"
-  version      = "1.27"
-
-  lifecycle {
-    prevent_destroy = false
-  }
-}
-
-resource "ovh_cloud_project_kube_nodepool" "orchid_workerslarge" {
-  service_name   = var.ovh_project_id
-  kube_id        = ovh_cloud_project_kube.orchid_cluster.id
-  name           = "workerslarge"
-  flavor_name    = "d2-8"
-  desired_nodes  = 2
-  min_nodes      = 2
-  max_nodes      = 2
-  monthly_billed = true # THIS IS A BIT DANGEROUS, BECAUSE PODS ARE BILLED IMMEDIATELY FOR A FULL MONTH, SO PLEASE DEACTIVATE THIS WHEN EXPERIMENTING
-  anti_affinity  = true
-  autoscale      = false
-
-  lifecycle {
-    prevent_destroy = false
-  }
 }
