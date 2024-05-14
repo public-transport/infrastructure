b75b6c7c737323f28fba07b8c7fc1ab3d48cfccb
Julius Tens <mail@juliustens.eu>
2024-05-14 02:30:43 +0200

    update kube-hetzner

--- cloud-resources/hetzner-k8s.tf
+++ cloud-resources/hetzner-k8s.tf
@@ -15,7 +15,7 @@ module "kube-hetzner" {
   providers            = { hcloud = hcloud }
   hcloud_token         = var.hetzner_cloud_token
   source               = "kube-hetzner/kube-hetzner/hcloud"
-  version              = "v2.12.0"
+  version              = "v2.13.5"
   create_kustomization = false
 
   ssh_public_key  = var.hetzner_k8s_ssh_public
